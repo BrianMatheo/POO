c6a1b10c096773b9f35ca07db344755243b9a26c
Brian Matheo Alvarez <bmalvarezp@ufpso.edu.co>
2024-02-29 15:23:28 -0500

    Creación de personajes

--- Ejercicio1/src/main/java/com/claseufpso/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/main/java/com/claseufpso/ejercicio1/Ejercicio1.java
@@ -1,16 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.claseufpso.ejercicio1;
 
-/**
- *
- * @author SCIS2PC14
- */
 public class Ejercicio1 {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Character homero = new Character("Homero",39,'M',"A la grande le puse cuca");
+        homero.saySomething();
+        
+        Character bart = new Character("Bart",10,'M',"Ay caramba!");
+        bart.saySomething();
+        
+        Character lisa = new Character("Lisa",8,'F',"de hecho");
+        lisa.saySomething();
+        
     }
-}
+        
+        static class Character{
+            String name;
+            int age;
+            char gender;
+            String phrase;
+
+            public Character(String name, int age, char gender, String phrase) {
+                this.name = name;
+                this.age = age;
+                this.gender = gender;
+                this.phrase = phrase;
+            }
+            
+            void saySomething(){
+                System.out.println(this.phrase);
+            }
+        }
+        
+    }
+
